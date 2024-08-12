61946473e07de90cbbeea3c9eac67dbbc3e07a1a
Nobody <bunnza2020@gmail.com>
2024-08-12 12:15:22 -0500

    Button Layout finally

--- StartSceen.py
+++ StartSceen.py
@@ -1,7 +1,8 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.app import App
 
-class TitleScreenWidget(BoxLayout):
+class TitleScreenWidget(AnchorLayout):
     pass
 
 class TitleScreenApp(App):
