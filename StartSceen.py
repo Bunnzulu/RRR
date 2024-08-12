022866a9698f25b003db4975272164932655f5a0
Nobody <bunnza2020@gmail.com>
2024-08-12 13:35:00 -0500

    ControlsWidget

--- StartSceen.py
+++ StartSceen.py
@@ -2,8 +2,18 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.app import App
 
+def Navigate_to_Page(Page):
+    app = App.get_running_app()
+    app.root.clear_widgets()
+    app.root.add_widget(Page)
+
 class TitleScreenWidget(AnchorLayout):
-    pass
+    def Goto_controls_page(self):
+        Navigate_to_Page(ControlsWidget())
+
+class ControlsWidget(BoxLayout):
+    def Back(self):
+        Navigate_to_Page(TitleScreenWidget())
 
 class TitleScreenApp(App):
     pass
--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from kivy.app import App
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.widget import Widget
 
+#Other files
+from StartSceen import TitleScreenWidget
+#--------
+
 
 
 class MainGameWidgets(Widget):
-    pass
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.StartScreen = TitleScreenWidget()
 
 
 
