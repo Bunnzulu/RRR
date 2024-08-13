7dda1f3ab017356299919a1bc6ddca175fdd32c3
Nobody <bunnza2020@gmail.com>
2024-08-13 15:53:58 -0500

    Update Levels.py

--- Levels.py
+++ Levels.py
@@ -1,10 +1,27 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 import pytmx
-
+from kivy.core.window import Window
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.core.image import Image as CoreImage
 
 class MapWidget(Widget):
-    pass
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.Load_Level("Graphics\Maps\Level1.tmx")
+        print(Window.width,Window.height)
+    def Load_Level(self,Level):
+        Map = pytmx.TiledMap(Level)
+        for layer in Map.layers:
+            for x,y,image in layer.tiles():
+                image = CoreImage(image[0]).texture
+                with self.canvas:
+                    Rectangle(pos=(x*1000/40,y),size=(32,32),texture=image)
+                    print(x*1000/40)
+    
+    def Tile_Transformation(x,y,layer):
+        pass
+    
 
 
 
